a25455ffaaad6cbedeec882c204d98b8d145295f
hamid ali <hamidali786offiun@gmail.com>
2022-12-16 09:01:18 -0800

    pf lab02

---
+++ cretivity.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+ main()
+{
+cout <<"                                                        " <<endl;
+cout <<"                                                        " <<endl;
+cout <<"     ***************************************************/\\  " <<endl;
+cout <<"     *   /     /     /    /    /    /     /     /      *  *   " <<endl;
+cout <<"     *  /     /     /    /    /    /     /     /      *    *  "  <<endl;
+cout <<"     * /     /     /    /    /    /     /     /      *      *  " <<endl;
+cout <<"     */     /     /    /    /    /     /     /      *        *  " <<endl;
+cout <<"     *_____________________________________________*__________*  " <<endl;
+cout <<"     :                                             :      !    : " <<endl;
+cout <<"     :                                             :      !    : " <<endl;
+cout <<"     :       %........%                            :      !    : " <<endl;
+cout <<"     :       %        %                            :      !    : " <<endl;
+cout <<"     :       %        %                            :      !    :  " <<endl;
+cout <<"     :       %        %                            :      !    : " <<endl;
+cout <<"     :       %........%                            :      !    : " <<endl;
+cout <<"     :                                             :      !    : " <<endl;
+cout <<"     :                                             :      !    : " <<endl;
+cout <<"     :                                             :      !    : " <<endl;
+cout <<"     :                                             :      !    :    " <<endl;
+cout <<"     :                                             :      !    :  " <<endl;
+cout <<"     :                                             :      !    :  " <<endl;
+cout <<"     :_____________________________________________:______!____:     " <<endl;
+
+cout <<"                                                     * ======= *  " <<endl;
+cout <<"                                                      * ========*    " <<endl;
+cout <<"                                                       * ========*   " <<endl;
+cout <<"                                                        * ======== *   " <<endl;
+cout <<"                                                         * ========*   " <<endl;
+cout <<"                                                          *         *   " <<endl;
+cout <<"                                                                  " <<endl;
+
+
+
+
+
+cout <<"                                               " <<endl;
+cout <<"                                               " <<endl;}
---
+++ project.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout <<"***********************************************" <<endl;
+cout <<"*            HAMID                            *"<<endl;
+cout <<"*        MEDICAL STORE                        *" <<endl;
+cout <<"*                                             *" <<endl;
+cout <<"*                                             *" <<endl;
+cout <<"***********************************************";
+}
